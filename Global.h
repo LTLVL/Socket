e915c5e6a053db70a325f7fbf05e7445ce1b60f9
Liu <1184690200@qq.com>
2023-12-05 20:41:07 +0800

    【修改描述】修改消息包结构

--- Global.h
+++ Global.h
@@ -1,76 +1,74 @@
-#ifndef __SOCKET_EXP_GLOBAL_H__
-#define __SOCKET_EXP_GLOBAL_H__
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <ctime>
-//
 #include <iostream>
 #include <string>
-//
 #include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/tcp.h>
-//
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-#define MAX_CLIENT_NUM 10
-#define READ_BUFFER_SIZE 1024
-#define DATA_LENGTH 256
-
 typedef struct {
-    int num;
+    int id;
     char ip[15];
     int port;
-    int isThisMyfd = 0;
-} client_info;
+} client;
 
-enum Operation {
+enum operation_type {
     CONNECT = 1,
     CLOSE,
     TIME,
     NAME,
     ACTIVE_LIST,
     MESSAGE,
-    EXIT,
+    EXIT
+};
+
+enum message_type {
+    REPLY = 1,
+    REQUIRE,
     ERROR
 };
 
-class PacketHeader {
+class PacketHead {
 public:
-    int source; //source fd
-    int destination; //destination fd
+    // 来源和目的文件描述符
+    int source_fd;
+    int destination_fd;
+    // data部分的内容长
     int length; //total length of data needed to pass;
-    int type; //1:require 0:reply 2:error
-    Operation op; //illustrate the operation(get name/time...) of this packet
+    // 消息类型
+    message_type type;
+    // 消息相关指令
+    operation_type operation;
 
-    PacketHeader() {}
-    PacketHeader(int sourece, int destination, int length, int type, Operation op)
-        : source(sourece)
-        , destination(destination)
-        , length(length)
-        , type(type)
-        , op(op)
+    PacketHead() {}
+    PacketHead(int source_fd, int destination_fd, int length, message_type type, operation_type operation)
+            : source_fd(source_fd)
+            , destination_fd(destination_fd)
+            , length(length)
+            , type(type)
+            , operation(operation)
     {
     }
 };
 
 typedef struct {
-    unsigned char data[DATA_LENGTH];
-    client_info list[MAX_CLIENT_NUM];
-} PacketData;
+    char data[256];
+    client client_list[10];
+} PacketBody;
 
 class Packet {
 public:
-    PacketHeader header;
-    PacketData body;
+    PacketHead header;
+    PacketBody body;
     Packet() {}
-    Packet(int sourece, int destination, int length, int type, Operation op, unsigned char* in_data)
+    Packet(int sourece, int destination, int length, message_type type, operation_type op, unsigned char* in_data)
         : header(sourece, destination, length, type, op)
     {
-        memset(body.data, 0, DATA_LENGTH);
+        memset(body.data, 0, 1024);
     }
 };
-#endif //__SOCKET_EXP_GLOBAL_H__
--- func.cpp
+++ func.cpp
@@ -15,8 +15,11 @@ using namespace std;
 
 
 void receiveData(int fd);
+
 void showMenu();
+
 int init();
+
 void checkQueue();
 
 deque<Packet> data_queue;
@@ -87,7 +90,7 @@ void menu() {
                     break;
                 }
                 //封装查询体
-                Packet to_send(fd, -1, sizeof(Packet), 1, TIME, nullptr);
+                Packet to_send(fd, -1, sizeof(Packet), REPLY, TIME, nullptr);
                 send(fd, &to_send, sizeof(Packet), 0);
                 //等待响应
                 while (data_queue.empty());
@@ -95,7 +98,7 @@ void menu() {
                 time_t time = -1;
 
                 for (auto iter = data_queue.begin(); iter != data_queue.end();) {
-                    if (iter->header.op == TIME) {
+                    if (iter->header.operation == TIME) {
                         time = *(time_t *) (iter->body.data);
                         data_queue.erase(iter);
                         break;
@@ -117,7 +120,7 @@ void menu() {
                     break;
                 }
                 //封装查询体
-                Packet to_send(fd, -1, sizeof(Packet), 1, NAME, nullptr);
+                Packet to_send(fd, -1, sizeof(Packet), REPLY, NAME, nullptr);
                 send(fd, &to_send, sizeof(Packet), 0);
                 //等待响应
                 while (data_queue.empty());
@@ -125,7 +128,7 @@ void menu() {
                 string name = "";
 
                 for (auto iter = data_queue.begin(); iter != data_queue.end();) {
-                    if (iter->header.op == NAME) {
+                    if (iter->header.operation == NAME) {
                         name = *(char *) (iter->body.data);
                         data_queue.erase(iter);
                         break;
@@ -147,7 +150,7 @@ void menu() {
                     break;
                 }
                 //封装查询体
-                Packet to_send(fd, -1, sizeof(Packet), 1, ACTIVE_LIST, nullptr);
+                Packet to_send(fd, -1, sizeof(Packet), REPLY, ACTIVE_LIST, nullptr);
                 send(fd, &to_send, sizeof(Packet), 0);
                 //等待响应
                 while (data_queue.empty());
@@ -159,15 +162,12 @@ void menu() {
                 cout << "num\tip\t\tport\t" << endl;
 
                 for (auto iter = data_queue.begin(); iter != data_queue.end();) {
-                    if (iter->header.op == ACTIVE_LIST) {
+                    if (iter->header.operation == ACTIVE_LIST) {
                         temp = *(int *) (iter->body.data);
                         for (int i = 0; i < temp; ++i) {
-                            if (iter->body.list[i].isThisMyfd == 1)
-                                cout << iter->body.list[i].num << " (me)\t";
-                            else
-                                cout << iter->body.list[i].num << "\t";
-                            cout << iter->body.list[i].ip << "\t";
-                            cout << iter->body.list[i].port << "\t" << endl;
+                            cout << iter->body.client_list[i].id << "\t";
+                            cout << iter->body.client_list[i].ip << "\t";
+                            cout << iter->body.client_list[i].port << "\t" << endl;
                         }
                         data_queue.erase(iter);
                         break;
@@ -197,36 +197,8 @@ void menu() {
                 cout << "Type your destination fd" << endl;
                 cin >> dest_fd;
 
-                Packet to_send(tr_fd, dest_fd, sizeof(Packet), 1, MESSAGE, nullptr);
+                Packet to_send(tr_fd, dest_fd, sizeof(Packet), REPLY, MESSAGE, nullptr);
                 send(fd, &to_send, sizeof(Packet), 0);
-
-                while (data_queue.empty());
-                string OK;
-                for (auto iter = data_queue.begin(); iter != data_queue.end();) {
-                    if (iter->header.op == MESSAGE && iter->header.type == 0) {
-                        OK = "ok";
-                        if (iter->header.source == dest_fd && OK == "ok"){
-                            cout << "Get message done!" << endl;
-                        }
-
-                        data_queue.erase(iter);
-                        break;
-                    } else if (iter->header.type == 2) {
-                        OK = "error";
-                        cout << "No such client linked" << endl;
-                        data_queue.erase(iter);
-                        break;
-                    } else {
-                        iter++;
-                    }
-                }
-
-                if (OK.empty()) {
-                    cout << "No send packet received " << endl;
-                } else {
-                    cout << "Receive a reply." << endl;
-                }
-
                 break;
             }
             case 7: {
@@ -265,76 +237,49 @@ int init() {
 
 void receiveData(int fd) {
     while (!stop) {
-        int flag = 1;
         memset(rec_buffer, 0, sizeof(rec_buffer));
 
-        int head_total = sizeof(Packet);
         int received = 0;
         int bytes = 0;
 
         Packet temp;
         //读取数据
         do {
-            bytes = read(fd, &temp, head_total - received);
+            bytes = read(fd, &temp, sizeof(Packet) - received);
+            //收到错误信息
+            if(temp.header.type == ERROR){
+                cout << "Received an error message" << endl;
+            }
+            if(temp.header.operation == MESSAGE){
+                string message = temp.body.data;
+                cout << "Receive message:\"" << message << "\" from " << temp.header.source_fd << endl;
+            }
             if (bytes < 0) {
-                perror("Error reading recv_buffer from socket");
-                flag = 0;
+                perror("Error reading from socket");
                 break;
             }
             if (bytes == 0) {
                 //the server is closed
-                cout << "the server is closed\n";
-                flag = 0;
+                cout << "The server is down." <<endl;
                 break;
             }
             received += bytes;
-        } while (received < head_total);
-
-        Packet *phead = (Packet *) rec_buffer;
-        int data_length = phead->header.length - sizeof(PacketHeader);
-
-        if (data_length > 0 && data_length > sizeof(Packet) - sizeof(PacketHeader)) {
-            //need extra read
-            received = 0;
-            do {
-                bytes = read(fd, rec_buffer + head_total + received, data_length - received);
-
-                if (bytes < 0)
-                    perror("Error reading rec_buffer from socket");
-                if (bytes == 0) //the server is closed
-                {
-                    break;
-                }
-                received += bytes;
-
-            } while (received < data_length);
-        }
+        } while (received < sizeof(Packet));
 
         data_queue.push_front(temp);
-        if (data_queue.front().header.op == MESSAGE && data_queue.front().header.type == 1) {
-            Packet to_send(data_queue.front().header.destination, data_queue.front().header.source, sizeof(Packet),
-                           0, MESSAGE, nullptr);
-            strncpy((char *) to_send.body.data, "ok", strlen("ok") + 1); //copy data
-            send(fd, &to_send, sizeof(Packet), 0);
-            cout << "destination:" << to_send.header.destination << "src " << to_send.header.source
-                 << " reply succuess\n";
-            data_queue.pop_front();
-        }
-        if (!flag) {
-            break;
-        }
+
     }
 }
 
-void checkQueue(){
+void checkQueue() {
     //等待响应
-    if (data_queue.empty()){
+    if (data_queue.empty()) {
         return;
     }
     string message;
 
     for (auto iter = data_queue.begin(); iter != data_queue.end();) {
-        if (iter->header.op == MESSAGE) {
+        if (iter->header.operation == MESSAGE) {
             message = *(char *) (iter->body.data);
             data_queue.erase(iter);
             break;
@@ -343,7 +288,7 @@ void checkQueue(){
         }
     }
 
-    if (!message.empty())  {
+    if (!message.empty()) {
         cout << message << endl;
     }
 }
